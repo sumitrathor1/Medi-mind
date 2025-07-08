56fb2486983d0e474f227b8812261784a4069b26
Sumit Rathor <sumitrathor142272@gmail.com>
2025-07-08 14:35:31 +0530

    Add Icon, head section, create vendots for main page

---
+++ assets/pages/_page-head.php
@@ -0,0 +1,27 @@
+   <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <meta name="robots" content="noindex, nofollow">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon/favicon.ico" rel="icon">
+    <link href="assets/img/favicon/apple-touch-icon.png" rel="apple-touch-icon">
+    <link rel="manifest" href="assets/img/favicon/site.webmanifest">
+
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendors/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- <link href="vendors/aos/aos.css" rel="stylesheet"> -->
+    <link href="vendors/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="vendors/swiper/swiper-bundle.min.css" rel="stylesheet">
---
+++ index.php
@@ -0,0 +1,6 @@
+<html lang="en">
+
+<head>
+    <?php include "assets/pages/_page-head.php" ?>
+    <title>Medimind</title>
+</head>
