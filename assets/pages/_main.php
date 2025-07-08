b1370db947f28b1dd61ae7b1ce274fa32708c025
Sumit Rathor <sumitrathor142272@gmail.com>
2025-07-08 15:45:10 +0530

    Add main page

---
+++ assets/pages/_main.php
@@ -0,0 +1,48 @@
+<main class="main d-flex w-100 h-100 justify-content-center align-items-center">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section w-100 h-100 d-flex justify-content-center align-items-center">
+        <div class="container aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="hero-content aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+
+                        <!-- NGO Tagline -->
+                        <div class="company-badge mb-4">
+                            <i class="bi bi-heart-pulse-fill me-2"></i>
+                            Medi-Mind Those Who Can't Reach Doctors
+                        </div>
+
+                        <!-- Main Headline -->
+                        <h1 class="mb-4">
+                            Emergency Medical <br>
+                            Advice at Your Doorstep <br>
+                            <span class="accent-text">By Medi-Mind</span>
+                        </h1>
+
+                        <!-- Short Description -->
+                        <p class="mb-4 mb-md-5">
+                            Medi-Mind is a thaught that helps people who can't visit doctors.
+                            We provide free symptom guidance, suggest basic medicines and basic medical knowledge
+                            and chat support — all for those in need.
+                        </p>
+
+                        <!-- CTA Buttons -->
+                        <div class="hero-buttons">
+                            <a href="single-symptoms-disease" class="btn btn-primary me-0 me-sm-2 mx-1">Single Symptoms Disease</a>
+                            <a href="multiple-symptoms-disease" class="btn btn-primary me-0 me-sm-2 mx-1">Multiple Symptoms Disease</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Right Side Image + Testimonial -->
+                <div class="col-lg-6">
+                    <div class="hero-image aos-init aos-animate" data-aos="zoom-out" data-aos-delay="300">
+                        <img src="assets/img/medi-Mind.png" alt="assets/img/medi-Mind.png" class="img-fluid"
+                            id="heroImage">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!-- /Hero Section -->
+</main>
