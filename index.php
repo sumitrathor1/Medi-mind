a30f8cf5dd366f6f31897a8054779485f4015af3
Sumit Rathor <sumitrathor142272@gmail.com>
2025-07-08 15:10:22 +0530

    Add main page and delete header

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
     <title>Medimind</title>
 </head>
 <body class="index-page scrolled" data-aos-easing="ease-in-out" data-aos-duration="600" data-aos-delay="0">
-    <?php include "assets/pages/_header.php"; ?>
-    <?php /* include "assets/pages/_main.php"; */?>
-    <?php/* include "assets/pages/_footer.php"; */?>
+    <?php  include "assets/pages/_main.php";?>
     <!-- Scroll Top -->
     <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center active">
         <i class="bi bi-arrow-up-short">
         </i>
     </a>
-    <?php /*include "assets/pages/_page-script.php"; */?>
+    <?php include "assets/pages/_page-script.php";?>
 </body>
 
 </html>
